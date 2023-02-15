f5785adaefe6635f836ee51704b92799022bb109
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-02-14 19:11:30 -0500

    Hey alex!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello class!!!");
+
+        System.out.println("kdaslkdhasjdhlajdkashiuasda");
     }
 
 }
