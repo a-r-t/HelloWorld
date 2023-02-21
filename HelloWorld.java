1dd3daf0209866b37af80bb09d942a224437ad9a
a-r-t <alexthimineur@gmail.com>
2023-02-21 18:31:56 -0500

    fix hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,8 @@
-import javax.swing.event.SwingPropertyChangeSupport;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello class!!!");
-
-        System.out.println("kdaslkdhasjdhlajdkashiuasda");
-        System.out.println("coding is so fun!!! I love it!!!");
     }
 
 }
