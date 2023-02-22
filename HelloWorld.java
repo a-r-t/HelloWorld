a82bc41318aa5bb75fc361b6da403d943a465260
a-r-t <alexthimineur@gmail.com>
2023-02-21 19:19:01 -0500

    programming is fun println

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello branchies!!!");
+        System.out.println("Programming is fun!!!");
     }
 
 }
